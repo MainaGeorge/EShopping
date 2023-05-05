2457c0b61a61b1426a25a6548e3262ef3584b578
George <nuriszka@gmail.com>
2023-05-05 15:07:50 +0200

    Add project files.

---
+++ Services/Catalog/Catalog.API/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace Catalog.API
+{
+    public class StartUp
+    {
+        private readonly IConfiguration _configuration;
+
+        public StartUp(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
