d9d9f278b11d7d04dfc6d961b89f05f3b54271ca
George <nuriszka@gmail.com>
2023-05-06 19:34:42 +0200

    added catalog repository

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<Product> GetById(string id);
+        Task<IEnumerable<Product>> GetProductsByBrand(string brand);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
---
+++ Services/Catalog/Catalog.Infrastructure/Data/ProductTypeContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class ProductTypeContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductType> typesCollection)
+        {
+            bool checkIfSeeded = typesCollection.Find(t => true).Any();
+            string path = Path.Combine("Data", "SeedData", "types.json");
+
+            if (checkIfSeeded) return;
+
+            var typesData = File.ReadAllText(path);
+            var types = JsonSerializer.Deserialize<IEnumerable<ProductType>>(typesData);
+
+            if (types == null) return;
+
+            foreach (var type in types)
+                typesCollection.InsertOneAsync(type);
+
+        }
+    }
+}
