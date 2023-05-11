d4f15879fd876e82293cd8b8894e448daee3bca7
George Maina <george.maina@finova.tech>
2023-05-11 14:47:55 +0200

    added basket  srvice to container

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,25 +1,35 @@
+using Discount.API.Services;
+using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Extensions;
+using Discount.Infrastructure.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssembly(typeof(GetDiscountQuery).GetTypeInfo().Assembly));
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<DiscountService>();
+    endpoints.MapGet("/", async context => await context.Response.WriteAsync("Communication with gRPC endpoints must be made through client"));
+});
 
 app.Run();
---
+++ Services/Discount/Discount.API/Services/DiscountService.cs
@@ -0,0 +1,69 @@
+﻿using Discount.Application.Commands;
+using Discount.Application.Queries;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.API.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IMediator mediator, ILogger<DiscountService> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var query = new GetDiscountQuery(request.ProductName);
+            var discount = await _mediator.Send(query);
+
+            _logger.LogInformation($"discount retrieved for product {request.ProductName}");
+            return discount;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new CreateDiscountCommand
+            {
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductName = request.Coupon.ProductName
+            };
+
+            var result = await _mediator.Send(cmd);
+            _logger.LogInformation($"discount created for product {result.ProductName}");
+
+            return result;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new UpdateDiscountCommand
+            {
+                Id = request.Coupon.Id,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductName = request.Coupon.ProductName
+            };
+
+            var result = await _mediator.Send(cmd);
+            _logger.LogInformation($"discount updated for product {result.ProductName}");
+
+            return result;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var cmd = new DeleteDiscountCommand(request.ProductName);
+
+            var deleted = await _mediator.Send(cmd);
+
+            return new DeleteDiscountResponse { Success = deleted };
+        }
+    }
+}
